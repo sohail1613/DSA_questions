f379aba64be41a6439e641de4a05358ec5d48aa7
Your Name <sohail1613@gmail.com>
2022-01-04 01:56:25 +0530

    Completed

--- src/com/company/Basic/Leapyear.java
+++ src/com/company/Basic/Leapyear.java
@@ -1,4 +1,17 @@
 package com.company.Basic;
 
+import java.util.Scanner;
+
 public class Leapyear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter year :");
+        int year= sc.nextInt();
+
+        if (((year%4==0)&&(year%100!=0)) || (year%100==0)){
+            System.out.println(year+" it is a given leap year");
+        } else {
+            System.out.println(year+" not a leap year");
+        }
+    }
 }
