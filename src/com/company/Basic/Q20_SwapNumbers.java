02e8a939bf4c774aecdb738e97f40e44016fa14b
Your Name <sohail1613@gmail.com>
2022-01-06 02:00:23 +0530

    Only variables used

--- src/com/company/Basic/Q20_SwapNumbers.java
+++ src/com/company/Basic/Q20_SwapNumbers.java
@@ -13,10 +13,18 @@ public class Q20_SwapNumbers {
         int number2 = sc.nextInt();
 
         // swapping numbers
-        int temp = number1;
-        number1=number2; // number2 becomes number1
+//        int temp = number1;
+//        number1=number2; // number2 becomes number1
+//        System.out.println("number1 :"+number1);
+//        number2=temp; //number1 becomes number2
+//        System.out.println("number2 :"+number2);
+
+        //swapping without using third variable
+        number1=number1+number2;  //5=5+10===>15
+        number2=number1-number2;  //10=15-10===>5
+        number1=number1-number2;  //15=15-5===>10
+
         System.out.println("number1 :"+number1);
-        number2=temp; //number1 becomes number2
         System.out.println("number2 :"+number2);
     }
 }
