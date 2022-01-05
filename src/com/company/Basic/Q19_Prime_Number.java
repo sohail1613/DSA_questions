c0abbef8d77e16c8632589105d57dd7cfdd72fec
Your Name <sohail1613@gmail.com>
2022-01-05 12:05:54 +0530

    incomplete solution

---
+++ src/com/company/Basic/Q19_Prime_Number.java
@@ -0,0 +1,23 @@
+//check given number is prime number or not.
+//https://thirdspacelearning.com/blog/what-is-a-prime-number/
+package com.company.Basic;
+
+import java.util.Scanner;
+
+public class Q19_Prime_Number {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter number :");
+        int number= sc.nextInt();  // asking input number from user
+
+        //applying condition if the given number is divisible by 1 & itself then only its prime
+        for (int i=2; i<number; i++){
+            if (number%i==0){
+                System.out.println("Not a prime number: "+number);
+            } else{
+                System.out.println("prime number: "+number);
+            }
+            return;
+        }
+    }
+}
