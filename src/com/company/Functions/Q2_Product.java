ea4bbf030059d92f5ce2041b5333a93b7ebec642
Your Name <sohail1613@gmail.com>
2022-01-06 16:22:29 +0530

    Product using Function

---
+++ src/com/company/Functions/Q2_Product.java
@@ -0,0 +1,23 @@
+//Print product of 2 numbers using Function (inputs from user)
+
+package com.company.Functions;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Q2_Product {
+    public static int calculateProduct(int number1, int number2){
+        return  (number1*number2); // we can directly without another variable
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number :");
+        int number1= sc.nextInt();
+        System.out.print("Enter second number :");
+        int number2= sc.nextInt();
+
+        // we can directly call Function into print statement.
+        System.out.println("Product of 2 numbers :"+calculateProduct(number1, number2));
+    }
+}
