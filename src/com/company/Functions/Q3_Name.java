f8dee93f5de8f7a313ee1e83d4c14e6bf86a681b
Your Name <sohail1613@gmail.com>
2022-01-06 16:29:51 +0530

    Print Name

---
+++ src/com/company/Functions/Q3_Name.java
@@ -0,0 +1,20 @@
+//write program to print name using function
+
+package com.company.Functions;
+
+import java.util.Scanner;
+
+public class Q3_Name {
+    public static void printMyName(String name){
+        System.out.print("Name is :"+name);
+        return;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter name :");
+        String name = sc.nextLine();
+
+        printMyName(name);
+    }
+}
