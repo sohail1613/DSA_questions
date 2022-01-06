7f4fa9557b5ee0ee9f0b049e0af6310bf6f9b85e
Your Name <sohail1613@gmail.com>
2022-01-06 16:11:26 +0530

    Solved using Function

---
+++ src/com/company/Functions/Q1_Sum.java
@@ -0,0 +1,22 @@
+//write a function to add 2 numbers
+
+package com.company.Functions;
+
+import java.util.Scanner;
+
+public class Q1_Sum {
+    public static int calculateSum(int number1, int number2){
+        int sum=number1+number2;
+        return  sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number :");
+        int number1= sc.nextInt();
+        System.out.print("Enter second number :");
+        int number2= sc.nextInt();
+        int sum=calculateSum(number1, number2); // call kiya function ko
+
+        System.out.println("Sum of 2 numbers is :"+sum);
+    }
+}
