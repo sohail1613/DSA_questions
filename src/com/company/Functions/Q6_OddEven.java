1e96c9d49d6297b211b8a028fd936a44f2e136e9
Your Name <sohail1613@gmail.com>
2022-01-07 00:43:56 +0530

    Odd and Even

---
+++ src/com/company/Functions/Q6_OddEven.java
@@ -0,0 +1,28 @@
+//Check the given number is odd or even using function.
+
+//Even = completely divisible by 2
+//Odd = not divisible by 2
+
+package com.company.Functions;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Q6_OddEven {
+    public static int checkOddEven(int number){
+        if (number%2==0){
+            System.out.println("Given number is even :"+number);
+        } else {
+            System.out.println("Given number is odd :"+number);
+        }
+        return number;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number :");
+        int number = sc.nextInt();
+
+        checkOddEven(number);
+    }
+}
