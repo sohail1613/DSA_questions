eb51e51500ed4029e1a25199f8e0441e3b1086c9
Your Name <sohail1613@gmail.com>
2022-01-08 02:19:02 +0530

    Digits product

---
+++ src/com/company/Functions/Q11_DigitsProduct.java
@@ -0,0 +1,23 @@
+package com.company.Functions;
+
+import java.util.Scanner;
+
+public class Q11_DigitsProduct {
+    public static int printDigitsProduct(int number){
+        int product=1;
+        while (number!=0){
+            product=product*(number%10);
+            number=number/10;
+        }
+        System.out.println("Products of digit is :"+product);
+        return  number;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter integer value :");
+        int number= sc.nextInt();
+
+        printDigitsProduct(number);
+    }
+}
