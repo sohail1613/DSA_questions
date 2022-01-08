67df951ac18d98c8ec9398e8d7821523563a17f9
Your Name <sohail1613@gmail.com>
2022-01-08 10:33:23 +0530

    Swapping

---
+++ src/com/company/Functions/Q13_Swap.java
@@ -0,0 +1,28 @@
+//Swap two numbers without using third variable.
+
+package com.company.Functions;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Q13_Swap {
+    public static void swapTwoNumbers(int number1, int number2){
+        number1=number1+number2;
+        number2=number1-number2;
+        number1=number1-number2;
+        System.out.println("After swapping numbers becomes :");
+        System.out.println("Number1 is :"+number1);
+        System.out.println("Number2 is: "+number2);
+        return ;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number1 :");
+        int number1= sc.nextInt();
+        System.out.print("Enter number2 :");
+        int number2 = sc.nextInt();
+
+        swapTwoNumbers(number1, number2);
+    }
+}
