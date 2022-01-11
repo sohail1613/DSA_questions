580775e076e9676897a893cc60077ba7c2ac2689
Your Name <sohail1613@gmail.com>
2022-01-12 01:04:56 +0530

    LCM without GCD

--- src/com/company/Basic/Q24_Lcm.java
+++ src/com/company/Basic/Q24_Lcm.java
@@ -7,8 +7,9 @@ import java.util.Scanner;
 public class Q24_Lcm {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number :");
+        System.out.print("Enter number1 :");
         int number1= sc.nextByte();
+        System.out.print("Enter number2 :");
         int number2= sc.nextByte();
 
         int large=Math.max(number1,number2);
@@ -16,7 +17,7 @@ public class Q24_Lcm {
 
         for (int i=large; ; i=i+large){
             if (i%small==0){
-                System.out.println("lcm is :"+i);
+                System.out.println("lcm of "+number1+" and "+number2+" is :"+i);
                 break;
             }
         }
