acddf2f34f2b2da4606ef0edbc680d29d03fb0e0
Your Name <sohail1613@gmail.com>
2022-01-11 23:56:10 +0530

    LCM without gcd

--- src/com/company/Functions/Q22_Lcm.java
+++ src/com/company/Functions/Q22_Lcm.java
@@ -1,5 +1,23 @@
 package com.company.Functions;
 
+import java.util.Scanner;
+
 public class Q22_Lcm {
-    public static int  
+    public static int LCM(int a, int b){
+        int large=Math.max(a,b);
+        int small=Math.min(a,b);
+
+        for (int i=large; ; i=i+large){
+            if (i%small==0)
+                return  i;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int a = sc.nextByte();
+        int b= sc.nextByte();
+
+        System.out.println("LCM of "+a+" and "+b+" is :"+LCM(a,b));
+    }
 }
