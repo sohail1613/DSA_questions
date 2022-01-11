5924d7e3efe6c1963579e14c71437acd2ef0fc10
Your Name <sohail1613@gmail.com>
2022-01-11 23:25:37 +0530

    Incomplete

---
+++ src/com/company/Basic/Q24_Lcm.java
@@ -0,0 +1,15 @@
+//find lcm without gcd
+
+package com.company.Basic;
+
+import java.util.Scanner;
+
+public class Q24_Lcm {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number :");
+        int a= sc.nextByte();
+        int b= sc.nextByte();
+
+    }
+}
