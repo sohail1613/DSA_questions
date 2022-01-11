729c4841c20b40764ce56363b78512b9e0f2fb3d
Your Name <sohail1613@gmail.com>
2022-01-12 04:53:14 +0530

    HCF & LCM

---
+++ src/com/company/Functions/Que23_LCMHCF.java
@@ -0,0 +1,36 @@
+//Find lcm and hcf is the given input.
+
+package com.company.Functions;
+
+import java.util.Scanner;
+
+public class Que23_LCMHCF {
+    public static void LCMHCF(int number1, int number2){
+        int temp1=number1;
+        int temp2=number2;
+        int lcm, hcf, temp;
+
+        while (temp2!=0){
+            temp=temp2;
+            temp2=temp1%temp2;
+            temp1=temp;
+        }
+
+        hcf=temp1;
+        lcm=(number1*number2)/hcf;
+
+        System.out.println("HCF of "+number1+" and "+number2+" is :"+hcf);
+        System.out.println("LCM of "+number1+" and "+number2+" is :"+lcm);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number:");
+        int number1= sc.nextInt();
+        System.out.print("Enter second number :");
+        int number2= sc.nextInt();
+
+        LCMHCF(number1, number2);
+    }
+}
