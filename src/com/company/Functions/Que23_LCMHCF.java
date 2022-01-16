a4fab86f49378c337ee31d39b929d57f1304bcb9
Your Name <sohail1613@gmail.com>
2022-01-16 10:54:14 +0530

    MOdified

--- src/com/company/Functions/Que23_LCMHCF.java
+++ src/com/company/Functions/Que23_LCMHCF.java
@@ -26,11 +26,13 @@ public class Que23_LCMHCF {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter first number:");
+        System.out.print("Enter first number :");
         int number1= sc.nextInt();
         System.out.print("Enter second number :");
         int number2= sc.nextInt();
+        int lcm, hcf;
 
         LCMHCF(number1, number2);
+
     }
 }
