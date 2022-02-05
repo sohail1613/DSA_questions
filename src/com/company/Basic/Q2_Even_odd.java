30e7626d36e7d143d307ff160828062e3dc51de9
Sohail <sohail1613@gmail.com>
2022-02-05 11:56:06 +0530

    Approach addded

--- src/com/company/Basic/Q2_Even_odd.java
+++ src/com/company/Basic/Q2_Even_odd.java
@@ -1,5 +1,13 @@
 //Find Even and odd numbers.
 
+//Approach
+//Leap Year contains 366 days, which comes once every four years. Every leap year corresponds to these facts :
+//
+//A century year is a year ending with 00. A century year is a leap year only if it is divisible by 400.
+//A leap year (except a century year) can be identified if it is exactly divisible by 4.
+//A century year should be divisible by 4 and 100 both.
+//A non-century year should be divisible only by 4.
+
 package com.company.Basic;
 
 import java.util.Scanner;
