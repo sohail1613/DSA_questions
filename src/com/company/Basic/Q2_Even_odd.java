c25e2e5ccae97eefd4130a9b5bdc42c4b667035b
Sohail <sohail1613@gmail.com>
2022-02-05 12:01:37 +0530

    Link added

--- src/com/company/Basic/Q2_Even_odd.java
+++ src/com/company/Basic/Q2_Even_odd.java
@@ -1,12 +1,5 @@
 //Find Even and odd numbers.
-
-//Approach
-//Leap Year contains 366 days, which comes once every four years. Every leap year corresponds to these facts :
-//
-//A century year is a year ending with 00. A century year is a leap year only if it is divisible by 400.
-//A leap year (except a century year) can be identified if it is exactly divisible by 4.
-//A century year should be divisible by 4 and 100 both.
-//A non-century year should be divisible only by 4.
+//https://www.geeksforgeeks.org/java-program-to-check-if-a-given-integer-is-odd-or-even/
 
 package com.company.Basic;
 
