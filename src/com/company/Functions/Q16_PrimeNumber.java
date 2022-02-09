33465d84f8787bcc96c08775435f2ede0e0175c0
Sohail <sohail1613@gmail.com>
2022-02-10 00:25:20 +0530

    Solution modified

--- src/com/company/Functions/Q16_PrimeNumber.java
+++ src/com/company/Functions/Q16_PrimeNumber.java
@@ -5,22 +5,27 @@ package com.company.Functions;
 import java.util.Scanner;
 
 public class Q16_PrimeNumber {
-    public static void checkPrimeNumber(int number){
-        for (int i=2; i<number; i++){
+    public static boolean isPrime(int number){
+        int i;
+        for (i=2; i<=number/2; i++){
             if (number%i==0){
-                System.out.println("Given number is not prime :"+number);
-            }else {
-                System.out.println("Given number is prime :"+number);
+                //System.out.println("Given number is not prime :"+number);
+                break;
+            }//else {
+               // System.out.println("Given number is prime :"+number);
             }
-            return;
+        return i>number/2;
         }
-    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number :");
         int number= sc.nextInt();
 
-        checkPrimeNumber(number);
+        if (isPrime(number)){
+            System.out.println(number+": it is a prime number.");
+        } else {
+            System.out.println(number+": it's not a prime number.");
+        }
     }
 }
