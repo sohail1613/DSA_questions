0755fe6971446620d79884ce5a6d22908837494a
Sohail <sohail1613@gmail.com>
2022-02-10 00:07:55 +0530

    Solution modified

--- src/com/company/Functions/Q17_PrintPrime.java
+++ src/com/company/Functions/Q17_PrintPrime.java
@@ -5,20 +5,24 @@ package com.company.Functions;
 import java.util.Scanner;
 
 public class Q17_PrintPrime {
-public static void printPrimeNumbers(int number) {
-    number = 100;
-    for (int i = 2; i < 100; i++) {
-        if (100 % i == 0) {
-            System.out.println("It's not prime :" + number);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number to check prime :");
+        int number= sc.nextInt();
+
+        if (isPrime(number)){
+            System.out.println(number+": is a prime number.");
         } else {
-            System.out.print("Prime numbers are :");
-            System.out.print(" "+ i + " ");
+            System.out.println(number+": is not prime number.");
         }
     }
-}
 
-    public static void main(String[] args) {
-        int number=100;
-        printPrimeNumbers(number);
+    public static boolean isPrime(int number){
+        int i;
+        for (i=2; i<=number/2; i++){
+            if (number%i==0)
+                break;
+        }
+        return i>number/2;
     }
 }
