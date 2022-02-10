af85db734f18eb551dd08a212822c18099054dcd
Sohail <sohail1613@gmail.com>
2022-02-10 23:12:53 +0530

    Solution modified

--- src/com/company/Arrays/Q6_AscendingOrder.java
+++ src/com/company/Arrays/Q6_AscendingOrder.java
@@ -5,5 +5,33 @@
 
 package com.company.Arrays;
 
+import java.util.Scanner;
+
 public class Q6_AscendingOrder {
+    public static void main(String[] args) {
+        Scanner aa = new Scanner(System.in);
+        System.out.print("Enter array size: ");
+        int size = aa.nextInt();
+
+        int order[] = new int[size];
+
+        for (int i=0 ; i<size; i++){
+            System.out.print("Enter number :");
+            order[i]= aa.nextInt();
+        }
+
+        boolean isAscending = true;
+
+        for (int i=0; i< order.length-1; i++){
+            if (order[i]>order[i+1]){
+                isAscending=false;
+            }
+        }
+
+        if (isAscending){
+            System.out.println("The array sorted in ascending order.");
+        } else {
+            System.out.println("The array sorted is descending order.");
+        }
+    }
 }
