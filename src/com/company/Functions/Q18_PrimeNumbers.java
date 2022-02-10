7394469cbf36627d362bb9e809326c97ba0f251a
Sohail <sohail1613@gmail.com>
2022-02-10 11:55:07 +0530

    Modified

--- src/com/company/Functions/Q18_PrimeNumbers.java
+++ src/com/company/Functions/Q18_PrimeNumbers.java
@@ -3,28 +3,22 @@
 package com.company.Functions;
 
 public class Q18_PrimeNumbers {
-    public  static void printPrimeNumbers(int number){
-        number=100;
-        int count;
-
-        for (int i=1; i<=number; i++){
-            count=0;
-            for (int j=2; j<=i/2; j++){
-                if (i%j==0){
-                    count ++;
-                    break;
-                }
-            }
-
-            if (count==0){
-                System.out.print(i+" ");
+    public  static boolean printPrimeNumbers(int number){
+        int i;
+        for (i=2 ; i<=number/2; i++){
+            if (number%i==0){
+                break;
             }
         }
-        return;
+        return i>number/2;
     }
 
     public static void main(String[] args) {
         int number=100;
-        printPrimeNumbers(number);  // call kiya function ko
+       printPrimeNumbers(number);  // call kiya function ko
+
+        if (printPrimeNumbers(number)){
+            System.out.println(number+": is prime");
+        }
     }
 }
