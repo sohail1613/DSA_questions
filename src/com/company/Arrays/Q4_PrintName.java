88382146ee781ffe43b2f2bb45d72afd9f139574
Sohail <sohail1613@gmail.com>
2022-02-10 16:19:03 +0530

    File added

---
+++ src/com/company/Arrays/Q4_PrintName.java
@@ -0,0 +1,29 @@
+//Take an array of names as input from the user and print them on the screen.
+
+package com.company.Arrays;
+
+import java.util.Scanner;
+
+public class Q4_PrintName {
+    public static void main(String[] args) {
+        Scanner eb = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int size = eb.nextInt();
+
+        //array
+        String name[] = new String[size];
+
+        //input
+        for (int i=0; i<size; i++){
+            System.out.print("Enter names : ");
+            name[i] = eb.next();
+        }
+
+
+        //output
+        for (int i=0; i< name.length; i++){
+            System.out.println("Name at index "+i+" is: "+name[i]);
+        }
+    }
+}
