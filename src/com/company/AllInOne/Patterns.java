1eeb1d5b461ace3e078ef4267e66331eac133fea
Sohail <sohail1613@gmail.com>
2022-02-14 18:52:16 +0530

    Dimaond pattern

--- src/com/company/AllInOne/Patterns.java
+++ src/com/company/AllInOne/Patterns.java
@@ -48,18 +48,15 @@ public class Patterns {
         System.out.println("Palindrome pattern.");
         palindrome(number1);
 
-        System.out.println("Diamond pattern.");
-        diamond(number1);
     }
 
 
-
     //solid square pattern
-    public static void solidSquare(int rowstar, int columnstar) {
+    public static void solidSquare(int row, int column) {
         //row stars
-        for (int i = 0; i <= rowstar; i++) {
+        for (int i = 0; i <= row; i++) {
             //column
-            for (int j = 1; j <= columnstar; j++) {
+            for (int j = 1; j <= column; j++) {
                 System.out.print("* "); //printing stars
             }
             System.out.println();  //print next line
@@ -67,13 +64,13 @@ public class Patterns {
     }
 
     //Hollow Square pattern.
-    public static void hollowSquare(int rowstar, int colstar){
+    public static void hollowSquare(int row, int column) {
         //outer stars
-        for (int i=0; i<=rowstar; i++){
+        for (int i = 0; i <= row; i++) {
             //column stars
-            for (int j=0; j<=colstar; j++){
+            for (int j = 0; j <= column; j++) {
                 //tracking boundary
-                if (i==0 || j==0 || i==rowstar || j==colstar){
+                if (i == 0 || j == 0 || i == row || j == column) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
@@ -84,9 +81,9 @@ public class Patterns {
     }
 
     //Solid Half pyramid pattern
-    public static void halfPyramid(int star){
-        for (int i=0; i<=star; i++){
-            for (int j=0; j<=i; j++){
+    public static void halfPyramid(int star) {
+        for (int i = 0; i <= star; i++) {
+            for (int j = 0; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
@@ -94,10 +91,10 @@ public class Patterns {
     }
 
     //Solid inverted half pyramid
-    public static void invertedHalfPyramid(int star){
-        for (int i=star; i>=1; i--){
+    public static void invertedHalfPyramid(int star) {
+        for (int i = star; i >= 1; i--) {
             //printing spaces
-            for (int j=1; j<=i; j++){
+            for (int j = 1; j <= i; j++) {
                 System.out.print("* "); //printing stars
             }
             System.out.println(); // printing next line
@@ -105,15 +102,15 @@ public class Patterns {
     }
 
     //Inverted half pyramid rotated.
-    public static void halfRotatedPyramid(int star){
-        for (int i=1; i<=star; i++){
+    public static void halfRotatedPyramid(int star) {
+        for (int i = 1; i <= star; i++) {
             //inner and printing spaces
-            for (int j=1; j<=star-i; j++){
+            for (int j = 1; j <= star - i; j++) {
                 System.out.print(" ");
             }
 
             //inner loop printing stars
-            for (int j=1; j<=i; j++){
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
 
@@ -123,29 +120,29 @@ public class Patterns {
     }
 
     //Numerical half pyramid
-    public static void numericalHalfPyramid(int number){
-        for (int i=1; i<=number; i++){  //outer loop
-            for (int j=1; j<=i; j++){  //inner loop
-                System.out.print(j+" ");  //printing numbers
+    public static void numericalHalfPyramid(int number) {
+        for (int i = 1; i <= number; i++) {  //outer loop
+            for (int j = 1; j <= i; j++) {  //inner loop
+                System.out.print(j + " ");  //printing numbers
             }
             System.out.println();  //printing next line
         }
     }
 
     //Inverted half numerical pyramid
-    public static void numericalHalfInvertedPyramid(int number){
-        for (int i=1; i<=number; i++){
-            for (int j=1; j<=number-i+1; j++){
-                System.out.print(j+" ");
+    public static void numericalHalfInvertedPyramid(int number) {
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= number - i + 1; j++) {
+                System.out.print(j + " ");
             }
             System.out.println();
         }
     }
 
     //Floyd's Triangle
-    public static void floydTriangle(int number){
-        for (int i=1; i<=number; i++){
-            for (int j=1; j<=i; j++){
+    public static void floydTriangle(int number) {
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print(number);
                 number++;
             }
@@ -154,14 +151,14 @@ public class Patterns {
     }
 
     //Binary pattern
-    public static void binaryPattern(int number){
-        for (int i=1; i<=number; i++){
-            for (int j=1; j<=i; j++){
-                int sum =i+j;
-                if (sum%2==0){ //even
-                    System.out.print(1+" ");
+    public static void binaryPattern(int number) {
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= i; j++) {
+                int sum = i + j;
+                if (sum % 2 == 0) { //even
+                    System.out.print(1 + " ");
                 } else { //odd
-                    System.out.print(0+" ");
+                    System.out.print(0 + " ");
                 }
             }
             System.out.println();
@@ -169,13 +166,13 @@ public class Patterns {
     }
 
     //Solid Rhombus
-    public static void solidRhombus(int star){
-        for (int i=1; i<=star; i++){
-            for (int j=1; j<=star-i; j++){ //printing spaces
+    public static void solidRhombus(int star) {
+        for (int i = 1; i <= star; i++) {
+            for (int j = 1; j <= star - i; j++) { //printing spaces
                 System.out.print(" ");
             }
             //printing stars
-            for (int j=1; j<=star; j++){
+            for (int j = 1; j <= star; j++) {
                 System.out.print("* ");
             }
             System.out.println();
@@ -183,52 +180,36 @@ public class Patterns {
     }
 
     //Number pyramid
-    public static void numberPyramid(int number){
-        for (int i=1; i<=number; i++){
+    public static void numberPyramid(int number) {
+        for (int i = 1; i <= number; i++) {
             //spaces
-            for (int j=1; j<=number-i; j++){
+            for (int j = 1; j <= number - i; j++) {
                 System.out.print(" ");
             }
             //printing numbers
-            for (int j=1; j<=i; j++){
-                System.out.print(i+" ");
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
             }
             System.out.println();
         }
     }
 
     //Palindrome pattern
-    public static void palindrome(int number){
-        for (int i=1; i<=number; i++){
-            for (int j=1; j<=number-i; j++){ //spcaes
+    public static void palindrome(int number) {
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= number - i; j++) { //spaces
                 System.out.print(" ");
             }
 
             //left side
-            for (int j=i; j>=i; j--){
+            for (int j = i; j >= i; j--) {
                 System.out.print(j);
             }
             //right side
-            for (int j=2; j<=i; j++){
+            for (int j = 2; j <= i; j++) {
                 System.out.print(j);
             }
             System.out.println();  //next line
-        }
-    }
-
-    //Diamond pattern
-    public static void diamond(int number){
-        //top part
-        for (int i=1; i<=number; i++){
-            for (int j=1; j<=number-i; j++){ //spaces
-                System.out.print(" ");
-            }
-
-            //printing stars
-            for (int j=1; j<=2*number-i; j++){
-                System.out.print("*");
-            }
-            System.out.println();
         }
     }
 }
