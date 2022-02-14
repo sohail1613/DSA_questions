6a9089e50e9523693182f4d5935110f842c45422
Sohail <sohail1613@gmail.com>
2022-02-14 14:15:37 +0530

    Binary pattern

--- src/com/company/AllInOne/Patterns.java
+++ src/com/company/AllInOne/Patterns.java
@@ -32,6 +32,9 @@ public class Patterns {
 
         System.out.println("Inverted numerical half pyramid");
         numericalHalfInvertedPyramid(number1);
+
+        System.out.println("Binary pattern");
+        binaryPattern(number1);
     }
 
 
@@ -135,6 +138,21 @@ public class Patterns {
         }
     }
 
-    //binary pattern
+    //Binary pattern
+    public static void binaryPattern(int number){
+        for (int i=1; i<=number; i++){
+            for (int j=1; j<=i; j++){
+                int sum =i+j;
+                if (sum%2==0){ //even
+                    System.out.print(1+" ");
+                } else { //odd
+                    System.out.print(0+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
     
+
 }
