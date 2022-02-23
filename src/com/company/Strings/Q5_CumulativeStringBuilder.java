e03c2d070cebf157c65cbd0589471b703b3ea8d4
Sohail <sohail1613@gmail.com>
2022-02-23 23:11:02 +0530

    Spelling rectify

--- src/com/company/Strings/Q5_CumulativeStringBuilder.java
+++ src/com/company/Strings/Q5_CumulativeStringBuilder.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class Q5_CumulativeStringBuilder {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter size of array: ");
+        System.out.print("Enter array size: ");
         int size= sc.nextInt();
         String array[] = new String[size];
 
