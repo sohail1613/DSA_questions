95e7c79b6c6a06d2aa5e02beb2c85e0ab99afdfe
Sohail <sohail1613@gmail.com>
2022-02-23 22:00:45 +0530

    Reverse string using StringBuilder

---
+++ src/com/company/Strings/Q4_ReverseString.java
@@ -0,0 +1,27 @@
+//Reverse string (using Sting Builder).
+
+package com.company.Strings;
+
+import java.util.Scanner;
+
+public class Q4_ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter world :");
+        String word = sc.nextLine();
+
+        StringBuilder sb = new StringBuilder(word);
+
+        for (int i=0; i<sb.length()/2; i++){
+            int front = i;
+            int back = sb.length()-1 -i;  //5-1-0 => 4
+
+            char frontChar = sb.charAt(front);
+            char backChar = sb.charAt(back);
+
+            sb.setCharAt(front, backChar);
+            sb.setCharAt(back, frontChar);
+        }
+        System.out.println("Reversed String is: "+sb);
+    }
+}
