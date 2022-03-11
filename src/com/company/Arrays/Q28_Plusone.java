8e7a6d9723a607005733fedfa18af25d0b82ca6d
Sohail <sohail1613@gmail.com>
2022-03-11 17:06:21 +0530

    Plus one

---
+++ src/com/company/Arrays/Q28_Plusone.java
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/plus-one/
+
+package com.company.Arrays;
+
+public class Q28_Plusone {
+    public static void main(String[] args) {
+
+    }
+    public int [] plusOne(int digits[]){
+
+    }
+}
