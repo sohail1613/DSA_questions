cd7cba521588f7b419eb185b5e2e9973f2662c34
Sohail <sohail1613@gmail.com>
2022-03-16 18:11:01 +0530

    Kth missing positive number

--- src/com/company/BinarySearch/Q10_MissingPositive.java
+++ src/com/company/BinarySearch/Q10_MissingPositive.java
@@ -1,5 +1,6 @@
 package com.company.BinarySearch;
 //https://leetcode.com/problems/kth-missing-positive-number/discuss/1854787/Easy
+//Approach: Increasing ordered array
 public class Q10_MissingPositive {
     public static void main(String[] args) {
         int array[] = {2, 3, 4, 7, 11};
@@ -8,6 +9,10 @@ public class Q10_MissingPositive {
     }
 
     public static int findKthPositive(int arr[], int k){
+//        if every element in the array > k , the answer would be k.
+//        So, for every element <= k
+//        we need to increment k. And since the array is in increasing order, you can break out
+//        of the loop on the first instance this condition fails.
         for (int i : arr){
             if (i <= k) k++;
             else break;
