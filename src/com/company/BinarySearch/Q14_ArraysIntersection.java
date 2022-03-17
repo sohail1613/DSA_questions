8b8dfd1772d91b17034615cb6caf68134e9c1f0c
Sohail <sohail1613@gmail.com>
2022-03-17 12:24:51 +0530

    349 & 350: Arrays intersection

--- src/com/company/BinarySearch/Q14_ArraysIntersection.java
+++ src/com/company/BinarySearch/Q14_ArraysIntersection.java
@@ -34,6 +34,9 @@ public class Q14_ArraysIntersection {
     }
 
 //    https://leetcode.com/problems/intersection-of-two-arrays-ii/
+//    Problem:
+//    Given two integer arrays nums1 and nums2, return an array of their intersection.
+//    Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
     public static int[] intersectionSecond(int[] nums1, int[] nums2) {
         if(nums1.length==0)
             return nums1;
