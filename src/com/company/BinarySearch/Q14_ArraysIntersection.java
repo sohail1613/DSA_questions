508f311417c51cd587f2598f5f0112b4508edad8
Sohail <sohail1613@gmail.com>
2022-03-17 12:21:13 +0530

    350. Intersection of arrays

--- src/com/company/BinarySearch/Q14_ArraysIntersection.java
+++ src/com/company/BinarySearch/Q14_ArraysIntersection.java
@@ -10,6 +10,8 @@ public class Q14_ArraysIntersection {
        int nums1[] = {1,2,2,1};
        int nums2[] = {2,2};
         System.out.println(Arrays.toString(intersection(nums1, nums2)));
+
+        System.out.println(Arrays.toString(intersectionSecond(nums1, nums2)));
     }
 
     private static int[] intersection(int[] nums1, int[] nums2) {
@@ -29,5 +31,32 @@ public class Q14_ArraysIntersection {
         int array[] = new int[res.size()];
         for(int i=0; i<res.size(); i++) array[i] = res.get(i);
         return array;
+    }
+
+//    https://leetcode.com/problems/intersection-of-two-arrays-ii/
+    public static int[] intersectionSecond(int[] nums1, int[] nums2) {
+        if(nums1.length==0)
+            return nums1;
+        if(nums2.length==0)
+            return nums2;
+
+        //sorting arrays
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        int i=0,j=0,k=0;
+
+        while(i<nums1.length && j<nums2.length){
+            if(nums1[i]<nums2[j] )
+                i++;
+            else if(nums2[j]<nums1[i])
+                j++;
+            else if(nums1[i]==nums2[j]){
+                nums1[k++]=nums1[i];
+                i++;
+                j++;
+            }
+        }
+        return Arrays.copyOfRange(nums1, 0, k);
     }
 }
