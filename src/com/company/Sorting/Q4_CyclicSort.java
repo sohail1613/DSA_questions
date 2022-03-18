5b40626dd4af508c5c37da835f454ff56b720a4d
Sohail <sohail1613@gmail.com>
2022-03-18 18:02:50 +0530

    Cyclic sorting

--- src/com/company/Sorting/Q4_CyclicSort.java
+++ src/com/company/Sorting/Q4_CyclicSort.java
@@ -7,11 +7,11 @@ import java.util.Arrays;
 public class Q4_CyclicSort {
     public static void main(String[] args) {
         int array[] = {5, 4, 3, 2, 1};
-        cyclic(array);
+        sorting(array);
         System.out.println(Arrays.toString(array));
     }
 
-    static void cyclic(int[] array) {
+    static void sorting(int[] array) {
         int i=0;
         while(i < array.length){
             int correct = array[i]-1;
