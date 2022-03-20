2d50979c9cbe8ebad5e7e2170fd0842299ad7521
Sohail <sohail1613@gmail.com>
2022-03-20 17:40:37 +0530

    Find Duplicates

---
+++ src/com/company/Sorting/Q217_ContainsDuplicate.java
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/contains-duplicate/
+
+package com.company.Sorting;
+
+import java.util.Arrays;
+
+public class Q217_ContainsDuplicate {
+    public static void main(String[] args) {
+        int nums[] = {1,1,1,3,3,4,3,2,4,2};
+        System.out.println(containsDuplicate(nums));
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length-1; i++) {
+            if (nums[i] == nums[i+1]) return true;
+        }
+        return false;
+    }
+}
