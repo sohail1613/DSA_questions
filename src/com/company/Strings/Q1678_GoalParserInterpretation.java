f42937963eb22bb9d7b48d4366de3fe4b7d62d98
Sohail <sohail1613@gmail.com>
2022-03-21 17:00:09 +0530

    Shuffle String

---
+++ src/com/company/Strings/Q1678_GoalParserInterpretation.java
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/goal-parser-interpretation/
+
+package com.company.Strings;
+
+public class Q1678_GoalParserInterpretation {
+    public static void main(String[] args){
+        String command = "G()(al)";
+        System.out.println(interpret(command));
+    }
+
+    public static String interpret(String command) {
+        StringBuilder sb = new StringBuilder(command.length());
+        for(int i=0; i<command.length(); i++){
+            if(command.charAt(i) == 'G') sb.append('G');
+            if(command.charAt(i) == '('){
+                if(command.charAt(i+1) == ')') {
+                    sb.append('o');
+                    i++;
+                } else{
+                    sb.append("al");
+                    i=i+3;
+                }
+            }
+        }
+//        converting to string type.
+        return sb.toString();
+    }
+}
