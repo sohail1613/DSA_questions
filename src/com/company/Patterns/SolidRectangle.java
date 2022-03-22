9633e4fbcccf07eebd35a04d7e2daf25d6cb55b7
Sohail <sohail1613@gmail.com>
2022-03-22 22:23:05 +0530

    Patterns

--- src/com/company/Patterns/SolidRectangle.java
+++ src/com/company/Patterns/SolidRectangle.java
@@ -7,7 +7,17 @@ public class SolidRectangle {
     public static void main(String[] args) {
         pattern1(5); //solid rectangle
         pattern2(5); //Sold half triangle.
+        pattern3(5); //Inverted solid pyramid
         pattern4(5); //Numerical half traingle
+    }
+
+    private static void pattern3(int n) {
+        for (int i = 0; i <= n; i++) {
+            for (int j=0; j<=n-i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 
     private static void pattern4(int n) {
