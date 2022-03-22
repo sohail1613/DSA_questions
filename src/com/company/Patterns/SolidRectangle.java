65df06062ecda4613ea4613a9b32eadbe5bd84d1
Sohail <sohail1613@gmail.com>
2022-03-22 22:13:28 +0530

    Patterns

--- src/com/company/Patterns/SolidRectangle.java
+++ src/com/company/Patterns/SolidRectangle.java
@@ -1,14 +1,21 @@
-//    *****
-//    *****
-//    *****
-//    *****
-//    *****
+//Print all possible patterns, questions link below
+//https://github.com/kunal-kushwaha/DSA-Bootcamp-Java/blob/main/assignments/09-patterns.md
 
 package com.company.Patterns;
 
 public class SolidRectangle {
     public static void main(String[] args) {
-        pattern1(5);
+        pattern1(5); //solid rectangle
+        pattern2(5); //Sold triangle.
+    }
+
+    private static void pattern2(int n) {
+        for (int i=0; i<=n; i++){
+            for (int j=0; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 
     private static void pattern1(int n) {
@@ -19,4 +26,6 @@ public class SolidRectangle {
             System.out.println();
         }
     }
+
+
 }
