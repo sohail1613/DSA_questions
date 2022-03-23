d856fddf03a9af015d451f8d6ec7ce4eacd1f54e
Sohail <sohail1613@gmail.com>
2022-03-23 17:50:37 +0530

    Triangle sum using Recursion

--- src/com/company/Recursion/Q1_TriangleSum.java
+++ src/com/company/Recursion/Q1_TriangleSum.java
@@ -2,14 +2,28 @@
 
 package com.company.Recursion;
 
+import java.util.Arrays;
+
 public class Q1_TriangleSum {
     public static void main(String[] args) {
         int array[] = {1, 2, 3, 4, 5};
         //triangle(array);
+        sumTriangle(array);
     }
 
-    static int smTriangle(int arr[]){
+    static void sumTriangle(int arr[]){
         //base condition
-        
+        if (arr.length < 1){
+            return;
+        }
+        int temp[] = new int[arr.length-1];
+        for (int i = 0; i < arr.length-1 ; i++) {
+            int x = arr[i] + arr[i+1];
+            temp[i] = x;
+        }
+
+        sumTriangle(temp);
+
+        System.out.println(Arrays.toString(arr));
     }
 }
