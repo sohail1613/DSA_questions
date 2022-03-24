1b2a8552ddedf562960659c9592332a7836f2dca
Sohail <sohail1613@gmail.com>
2022-03-24 22:51:27 +0530

    Using XOR Operator

---
+++ src/com/company/Bitwise/FindUnique.java
@@ -0,0 +1,18 @@
+package com.company.Bitwise;
+
+
+public class FindUnique {
+    public static void main(String[] args) {
+        int array[] = {2, 3, 3, 4, 2, 6, 4};
+        System.out.println(ans(array));
+    }
+
+    private static int ans(int[] array) {
+        int unique=0;
+        for (int n: array){
+            unique ^= n; //using XOR gate (Exclusive OR)
+        }
+
+        return unique;
+    }
+}
