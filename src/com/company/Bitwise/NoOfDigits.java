62536b1a4ae5b5b821987436678be81fe659d144
Sohail <sohail1613@gmail.com>
2022-03-24 22:39:42 +0530

    Number of Digits

---
+++ src/com/company/Bitwise/NoOfDigits.java
@@ -0,0 +1,11 @@
+package com.company.Bitwise;
+
+public class NoOfDigits {
+    public static void main(String[] args) {
+        int n =10;
+        int b = 2;
+
+        int ans = (int)(Math.log(n) / Math.log(b)) + 1;
+        System.out.println(ans);
+    }
+}
