745cc4f4746a190bd244a857b5477d2bc41a7124
Sohail <sohail1613@gmail.com>
2022-03-24 23:50:17 +0530

    Exclusive OR gate

---
+++ src/com/company/Bitwise/RangeXOR.java
@@ -0,0 +1,33 @@
+package com.company.Bitwise;
+
+public class RangeXOR {
+    public static void main(String[] args) {
+        // range xor for a, b = xor(b) ^ xor(a-1)
+        int a =3, b=9;
+        int ans = xor(b) ^ xor(a-1);
+        System.out.println(ans);
+
+        // only for check, will give the for large numbers
+        int ans2 = 0;
+        for (int i = 0; i <= b; i++) {
+            ans2 ^= 1;
+        }
+        System.out.println(ans2);
+    }
+
+    //this gives XOR from 0 to a.
+    static int xor(int a){
+        if (a % 4 == 0){
+            return a;
+        }
+
+        if (a % 4 == 1){
+            return 1;
+        }
+
+        if (a % 4 == 2){
+            return a+1;
+        }
+        return 0;
+    }
+}
