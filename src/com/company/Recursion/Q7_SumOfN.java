68692d8b7fbb08f15592d7c6a09e539d6f2ba17b
Sohail <sohail1613@gmail.com>
2022-03-26 01:15:22 +0530

    Recursion sum

---
+++ src/com/company/Recursion/Q7_SumOfN.java
@@ -0,0 +1,15 @@
+package com.company.Recursion;
+
+public class Q7_SumOfN {
+    public static void main(String[] args) {
+        System.out.println(sum(5));
+    }
+
+    static int sum(int n){
+        if (n <= 1){
+            return 1;
+        }
+
+        return n + sum(n-1);
+    }
+}
