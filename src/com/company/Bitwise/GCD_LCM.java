55474604951a80efa84215943d01e6a9fb0f43de
Sohail <sohail1613@gmail.com>
2022-03-25 16:05:59 +0530

    GCD and LCM

---
+++ src/com/company/Bitwise/GCD_LCM.java
@@ -0,0 +1,18 @@
+package com.company.Bitwise;
+
+public class GCD_LCM {
+    public static void main(String[] args) {
+        System.out.println(gcd(2, 7));
+    }
+
+    static int gcd(int a, int b){
+        if (a == 0){
+            return b;
+        }
+        return gcd(b%a, a);
+    }
+
+    static int lcm(int a, int b){
+        return (a*b)/gcd(a, b);
+    }
+}
