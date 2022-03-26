5a1eaebb180680b1abbe508a4a166093fe8d1540
Sohail <sohail1613@gmail.com>
2022-03-26 12:13:03 +0530

    Recursion

---
+++ src/com/company/Recursion/Q11_CountZeros.java
@@ -0,0 +1,26 @@
+//Count number of zeros and print.
+
+package com.company.Recursion;
+
+
+public class Q11_CountZeros {
+    public static void main(String[] args) {
+        System.out.println(count(1083095060));
+    }
+
+    static int count(int n){
+        return helper(n, 0);
+    }
+
+    private static int helper(int n, int c){
+        if (n == 0){
+            return c;
+        }
+
+        int rem = n % 10;
+        if (rem == 0){
+            return helper(n/10, c+1);
+        }
+        return helper(n/10, c);
+    }
+}
