67436a6ca56027dec771c852d441888d8311e92a
Sohail <sohail1613@gmail.com>
2022-03-27 18:31:02 +0530

    Sorted Recursion

---
+++ src/com/company/Recursion/Q17_Sorted.java
@@ -0,0 +1,17 @@
+package com.company.Recursion;
+
+public class Q17_Sorted {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 5, 16, 8};
+        System.out.println(sorted(arr, 0));
+    }
+
+    static boolean sorted(int[] arr, int index) {
+        // base condition
+        if (index == arr.length - 1) {
+            return true;
+        }
+
+        return arr[index] < arr[index + 1] && sorted(arr, index + 1);
+    }
+}
