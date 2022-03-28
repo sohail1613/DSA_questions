6bac7a6ab7644bd12709d50e3843bb70073b8a16
Sohail <sohail1613@gmail.com>
2022-03-28 15:41:23 +0530

    Selection Recursion

--- src/com/company/Recursion/Q18_Pattern1.java
+++ src/com/company/Recursion/Q18_Pattern1.java
@@ -10,6 +10,9 @@ public class Q18_Pattern1 {
         int array[]={4, 3, 2, 1};
         bubble(array, array.length-1, 0);
         System.out.println(Arrays.toString(array));
+
+        selection(array, array.length, 0, 0);
+        System.out.println(Arrays.toString(array));
     }
 
     static void pattern(int row, int col){
@@ -58,6 +61,26 @@ public class Q18_Pattern1 {
             bubble(arr, row, col+1);
         } else{
             bubble(arr, row-1, 0);
+        }
+    }
+
+    static void selection(int array[], int row, int col, int max){
+        if (row == 0){
+            return;
+        }
+
+        if (col < row){
+            if (array[col] > array[max]){
+                selection(array, row, col+1, col);
+            } else{
+                selection(array, row, col+1, max);
+            }
+        } else{
+            int temp = array[max];
+            array[max] = array[row-1];
+            array[row-1] = temp;
+
+            selection(array, row-1, 0, 0);
         }
     }
 }
