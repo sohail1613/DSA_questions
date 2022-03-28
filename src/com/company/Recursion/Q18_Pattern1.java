d3ecc943adb52ebaa19b8dcf8c3f040ffbe4b7f9
Sohail <sohail1613@gmail.com>
2022-03-28 15:20:14 +0530

    Recursion

--- src/com/company/Recursion/Q18_Pattern1.java
+++ src/com/company/Recursion/Q18_Pattern1.java
@@ -1,8 +1,13 @@
 package com.company.Recursion;
 
+import java.util.Arrays;
+
 public class Q18_Pattern1 {
     public static void main(String[] args) {
         pattern(4, 0);
+        int array[]={4, 3, 2, 1};
+        bubble(array, array.length-1, 0);
+        System.out.println(Arrays.toString(array));
     }
 
     static void pattern(int row, int col){
@@ -16,6 +21,27 @@ public class Q18_Pattern1 {
         } else {
             System.out.println();
             pattern(row-1, 0);
+        }
+    }
+
+
+    static void bubble(int arr[], int row, int col){
+        if (row == 0){
+            return;
+        }
+
+        if (col<row){
+
+            if (arr[col] > arr[col+1]){
+                //swap
+                int temp = arr[col];
+                arr[col] = arr[col+1];
+                arr[col+1] = temp;
+            }
+
+            bubble(arr, row, col+1);
+        } else{
+            bubble(arr, row-1, 0);
         }
     }
 }
