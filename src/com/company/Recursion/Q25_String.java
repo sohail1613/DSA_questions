233dc32c58b3030df699cd08b4bbebf24b97485e
Sohail <sohail1613@gmail.com>
2022-03-30 13:01:48 +0530

    Recursive String

--- src/com/company/Recursion/Q25_String.java
+++ src/com/company/Recursion/Q25_String.java
@@ -4,7 +4,9 @@ package com.company.Recursion;
 
 public class Q25_String {
     public static void main(String[] args) {
-        skip("", "bacccahd");
+        skip("", "adari");
+        System.out.println(skipReturn("ankara"));
+
     }
 
     static void skip(String p, String up){
@@ -19,6 +21,20 @@ public class Q25_String {
             skip(p, up.substring(1));
         } else {
             skip(p+ch, up.substring(1));
+        }
+    }
+
+    static String skipReturn(String up){
+        if (up.isEmpty()){
+            return "";
+        }
+
+        char ch = up.charAt(0);
+
+        if (ch == 'a'){
+            return skipReturn(up.substring(1));
+        } else {
+            return ch + skipReturn(up.substring(1));
         }
     }
 }
