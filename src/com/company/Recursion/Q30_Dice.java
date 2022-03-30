5b94fe04ff9a2384251b8515f3d0ab774abe9dd9
Sohail <sohail1613@gmail.com>
2022-03-31 00:58:47 +0530

    Amazon

---
+++ src/com/company/Recursion/Q30_Dice.java
@@ -0,0 +1,19 @@
+//Amazon question
+
+package com.company.Recursion;
+
+public class Q30_Dice {
+    public static void main(String[] args) {
+        dice("", 4);
+    }
+
+    static void dice(String p, int target){
+        if (target == 0){
+            System.out.println(p);
+            return;
+        }
+        for (int i = 1; i <= 6 && i<= target ; i++) {
+            dice(p+i, target -i);
+        }
+    }
+}
