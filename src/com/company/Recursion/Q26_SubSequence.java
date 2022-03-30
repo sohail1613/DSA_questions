d63ff39699fe02e92115622de5452d492aedc380
Sohail <sohail1613@gmail.com>
2022-03-30 16:31:05 +0530

    ArrayList

--- src/com/company/Recursion/Q26_SubSequence.java
+++ src/com/company/Recursion/Q26_SubSequence.java
@@ -1,8 +1,11 @@
 package com.company.Recursion;
 
+import java.util.ArrayList;
+
 public class Q26_SubSequence {
     public static void main(String[] args) {
         subSeq("", "abc");
+        System.out.println(subSeqList("", "abc"));
     }
 
     static void subSeq(String p, String up){
@@ -15,5 +18,21 @@ public class Q26_SubSequence {
         char ch = up.charAt(0);
         subSeq(p+ch, up.substring(1));
         subSeq(p, up.substring(1));
+    }
+
+    static ArrayList<String> subSeqList(String p, String up){
+        //base condition
+        if (up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        char ch = up.charAt(0);
+        ArrayList<String> left = subSeqList(p+ch, up.substring(1));
+        ArrayList<String> right = subSeqList(p, up.substring(1));
+
+        left.addAll(right);
+        return left;
     }
 }
