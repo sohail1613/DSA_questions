248f58e798080cbdf1a8ce2bb5af34f2f97e0e62
Sohail <sohail1613@gmail.com>
2022-03-31 00:46:24 +0530

    Google Question

--- src/com/company/Recursion/Q29_Phonepad.java
+++ src/com/company/Recursion/Q29_Phonepad.java
@@ -6,6 +6,7 @@ public class Q29_Phonepad {
     public static void main(String[] args) {
         pad("", "12");
         System.out.println(padRet("", "12"));
+        System.out.println(padCount("", "12"));
     }
 
     static void pad(String p, String up){
@@ -37,5 +38,18 @@ public class Q29_Phonepad {
             list.addAll(padRet(p+ch, up.substring(1)));
         }
         return list;
+    }
+
+    static int padCount(String p, String up){
+        if (up.isEmpty()){
+            return 1;
+        }
+        int count = 0;
+        int digit = up.charAt(0) - '0';
+        for (int i = (digit-1)*3 ; i < digit*3 ; i++) {
+            char ch = (char)('a'+i);
+            count = count + padCount(p+ch, up.substring(1));
+        }
+        return count;
     }
 }
