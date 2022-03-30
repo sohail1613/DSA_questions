558821d1ef4ed38901c692b7e592111e2e7b7d2e
Sohail <sohail1613@gmail.com>
2022-03-31 00:42:03 +0530

    Google Que

--- src/com/company/Recursion/Q29_Phonepad.java
+++ src/com/company/Recursion/Q29_Phonepad.java
@@ -1,8 +1,11 @@
 package com.company.Recursion;
 
+import java.util.ArrayList;
+
 public class Q29_Phonepad {
     public static void main(String[] args) {
         pad("", "12");
+        System.out.println(padRet("", "12"));
     }
 
     static void pad(String p, String up){
@@ -17,5 +20,22 @@ public class Q29_Phonepad {
             char ch = (char)('a'+i);
             pad(p+ch, up.substring(1));
         }
+    }
+
+    static ArrayList<String> padRet(String p, String up){
+        //base condition
+        if (up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        int  digit = up.charAt(0) - '0'; //this will convert '2' into 2
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = (digit-1)*3; i < digit*3; i++) {
+            char ch = (char)('a'+i);
+            list.addAll(padRet(p+ch, up.substring(1)));
+        }
+        return list;
     }
 }
