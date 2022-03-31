d6d6ce80d0ef5cb415e11f79ae623717867cb774
Sohail <sohail1613@gmail.com>
2022-03-31 12:50:56 +0530

    Amazon

--- src/com/company/Recursion/Q30_Dice.java
+++ src/com/company/Recursion/Q30_Dice.java
@@ -2,9 +2,12 @@
 
 package com.company.Recursion;
 
+import java.util.ArrayList;
+
 public class Q30_Dice {
     public static void main(String[] args) {
         dice("", 4);
+        System.out.println(diceReturn("", 4));
     }
 
     static void dice(String p, int target){
@@ -15,5 +18,19 @@ public class Q30_Dice {
         for (int i = 1; i <= 6 && i<= target ; i++) {
             dice(p+i, target -i);
         }
+    }
+
+    //returning answer in list
+    static ArrayList<String> diceReturn(String p, int target){
+        if (target == 0){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 1; i <= 6 && i<= target ; i++) {
+            list.addAll(diceReturn(p+i, target-i));
+        }
+        return list;
     }
 }
