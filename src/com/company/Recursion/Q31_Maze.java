7087578cf3e80d749414bbb92251f122dcb8f4af
Sohail <sohail1613@gmail.com>
2022-03-31 22:43:45 +0530

    Maze

--- src/com/company/Recursion/Q31_Maze.java
+++ src/com/company/Recursion/Q31_Maze.java
@@ -8,6 +8,12 @@ public class Q31_Maze {
         path("", 3, 3);
         System.out.println(pathRet("", 3, 3));
         System.out.println(pathRetDiagonal("", 3, 3));
+        boolean board[][] = {
+                {true, true, true},
+                {true, false, true},
+                {true, true, true}
+        };
+        pathRestrictions("", board, 0, 0);
     }
 
     static int count(int r, int c){
@@ -69,4 +75,22 @@ public class Q31_Maze {
         }
         return list;
     }
+
+    static void pathRestrictions(String p, boolean[][] maze, int r, int c) {
+        if (r == maze.length - 1 && c == maze[0].length - 1) {
+            System.out.println(p);
+            return;
+        }
+
+        if (!maze[r][c]) {
+            return;
+        }
+        if (r < maze.length - 1) {
+            pathRestrictions(p + 'D', maze, r+1, c);
+        }
+        if (c < maze[0].length - 1) {
+            pathRestrictions(p + 'R', maze, r, c+1);
+        }
+    }
+
 }
