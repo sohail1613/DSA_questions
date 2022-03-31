7897a14b9a98ddfedad9f26e331636c7c3293d12
Sohail <sohail1613@gmail.com>
2022-03-31 22:15:25 +0530

    Maze

--- src/com/company/Recursion/Q31_Maze.java
+++ src/com/company/Recursion/Q31_Maze.java
@@ -3,6 +3,7 @@ package com.company.Recursion;
 public class Q31_Maze {
     public static void main(String[] args) {
         System.out.println(count(3, 3));
+        path("", 3, 3);
     }
 
     static int count(int r, int c){
@@ -12,5 +13,19 @@ public class Q31_Maze {
         int left = count(r-1, c);
         int right = count(r, c-1);
         return left+right;
+    }
+
+    static void path(String p, int r, int c){
+        if (r==1 && c==1){
+            System.out.println(p);
+            return;
+        }
+
+        if (r > 1){
+            path(p+'D', r-1, c);
+        }
+        if (c > 1){
+            path(p+'R', r, c-1);
+        }
     }
 }
