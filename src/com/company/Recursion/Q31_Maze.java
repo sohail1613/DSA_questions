749e715cc1a9137b0bfb0ad7775e3518e6837f45
Sohail <sohail1613@gmail.com>
2022-03-31 22:40:54 +0530

    Maze

--- src/com/company/Recursion/Q31_Maze.java
+++ src/com/company/Recursion/Q31_Maze.java
@@ -7,6 +7,7 @@ public class Q31_Maze {
         System.out.println(count(3, 3));
         path("", 3, 3);
         System.out.println(pathRet("", 3, 3));
+        System.out.println(pathRetDiagonal("", 3, 3));
     }
 
     static int count(int r, int c){
@@ -45,6 +46,26 @@ public class Q31_Maze {
         }
         if (c > 1){
             list.addAll(pathRet(p+'R', r, c-1));
+        }
+        return list;
+    }
+
+    static ArrayList<String> pathRetDiagonal(String p, int r, int c) {
+        if (r == 1 && c == 1) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+        if (r > 1 && c > 1) {
+            list.addAll(pathRetDiagonal(p + 'D', r-1, c-1));
+        }
+        if (r > 1) {
+            list.addAll(pathRetDiagonal(p + 'V', r-1, c));
+        }
+        if (c > 1) {
+            list.addAll(pathRetDiagonal(p + 'H', r, c-1));
         }
         return list;
     }
