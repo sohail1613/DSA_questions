db068103e8e30caa55b5339823796f328d85e801
Sohail <sohail1613@gmail.com>
2022-03-31 22:38:00 +0530

    Maze

--- src/com/company/Recursion/Q31_Maze.java
+++ src/com/company/Recursion/Q31_Maze.java
@@ -1,9 +1,12 @@
 package com.company.Recursion;
 
+import java.util.ArrayList;
+
 public class Q31_Maze {
     public static void main(String[] args) {
         System.out.println(count(3, 3));
         path("", 3, 3);
+        System.out.println(pathRet("", 3, 3));
     }
 
     static int count(int r, int c){
@@ -27,5 +30,22 @@ public class Q31_Maze {
         if (c > 1){
             path(p+'R', r, c-1);
         }
+    }
+
+    static ArrayList<String> pathRet(String p, int r, int c){
+        if (r==1 && c==1){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+        if (r > 1){
+            list.addAll(pathRet(p+'D', r-1, c));
+        }
+        if (c > 1){
+            list.addAll(pathRet(p+'R', r, c-1));
+        }
+        return list;
     }
 }
