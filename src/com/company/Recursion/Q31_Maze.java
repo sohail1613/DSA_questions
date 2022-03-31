b4a4f2fc8ea1ae1780cfe26581df1a51c3a64258
Sohail <sohail1613@gmail.com>
2022-03-31 22:11:43 +0530

    Maze

---
+++ src/com/company/Recursion/Q31_Maze.java
@@ -0,0 +1,16 @@
+package com.company.Recursion;
+
+public class Q31_Maze {
+    public static void main(String[] args) {
+        System.out.println(count(3, 3));
+    }
+
+    static int count(int r, int c){
+        if (r==1 || c==1){
+            return 1;
+        }
+        int left = count(r-1, c);
+        int right = count(r, c-1);
+        return left+right;
+    }
+}
