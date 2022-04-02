906d0e459a52f88b5354bde66855fb54caf82f60
Sohail <sohail1613@gmail.com>
2022-04-02 16:36:44 +0530

    342.Power of 4

---
+++ src/com/company/Recursion/Q42_PwerOfFour.java
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/power-of-four/
+
+package com.company.Recursion;
+
+public class Q42_PwerOfFour {
+    public static void main(String[] args) {
+        System.out.println(isPowerOfFour(14));
+    }
+
+    public static boolean isPowerOfFour(int n) {
+        if( n <= 0) return false;
+        if(n == 1) return true;
+        if(n % 4 != 0) return false;
+
+        return isPowerOfFour(n/4);
+    }
+}
