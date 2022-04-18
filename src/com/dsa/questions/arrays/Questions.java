af06045ede6f80a90403df354011b254ba754bd9
Sohail <sohail1613@gmail.com>
2022-04-19 01:41:15 +0530

    Array Permutation

--- src/com/dsa/questions/arrays/Questions.java
+++ src/com/dsa/questions/arrays/Questions.java
@@ -2,13 +2,13 @@ package com.dsa.questions.arrays;
 
 public class Questions {
 //    //Q1_Build Array from Permutation
-//    public int[] buildArray(int[] arr){
-//        int ans[] = new int[arr.length];
-//        for (int i = 0; i < arr.length ; i++) {
-//            ans[i] = arr[arr[i]];
-//        }
-//        return ans;
-//    }
+    public int[] buildArray(int[] arr){
+        int ans[] = new int[arr.length];
+        for (int i = 0; i < arr.length ; i++) {
+            ans[i] = arr[arr[i]];
+        }
+        return ans;
+    }
 
 //    Q2_Concatenation of Array
     public int[] concatenate(int[] array1){
@@ -46,4 +46,6 @@ public class Questions {
         }
         return runningSum;
     }
+
+
 }
