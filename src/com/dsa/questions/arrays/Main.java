c85e5a30e8e7689daa06eb2226860c05bee19d2e
Sohail <sohail1613@gmail.com>
2022-04-18 19:39:44 +0530

    Two array concatenation

--- src/com/dsa/questions/arrays/Main.java
+++ src/com/dsa/questions/arrays/Main.java
@@ -4,10 +4,17 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-//        Questions que = new Questions();
+        Questions que = new Questions();
 //
 //        int[] array = {0,2,1,5,3,4};
 //        int[] ans = que.buildArray(array);
 //        System.out.println(Arrays.toString(ans));
+
+        int[] array1 = {1, 2, 3, 4, 5};
+//        int[] answer = que.concatenate(array1);
+//        System.out.println(Arrays.toString(answer));
+        int[] arr = {6, 7, 8, 9};
+        int[] answer = que.concat(array1, arr);
+        System.out.println(Arrays.toString(answer));
     }
 }
--- src/com/dsa/questions/arrays/Questions.java
+++ src/com/dsa/questions/arrays/Questions.java
@@ -10,5 +10,28 @@ public class Questions {
 //        return ans;
 //    }
 
-    
+//    Q2_Concatenation of Array
+    public int[] concatenate(int[] array1){
+        int[] array= new int[2*array1.length];
+        for (int i=0; i< array1.length; i++){
+            array[i] = array1[i];
+            array[i+ array1.length] = array1[i];
+        }
+        return array;
+    }
+
+    //using two different array for concatenation
+    public  int[] concat(int[] arr1, int[] arr2){
+        int[] arr3 = new int[arr1.length+ arr2.length];
+        int i=0;
+        for (int element: arr1){
+            arr3[i] = element;
+            i++;
+        }
+        for (int element: arr2){
+            arr3[i] = element;
+            i++;
+        }
+        return arr3;
+    }
 }
