fa70811c551ed1ff0a395d59d15ef5a7efc9bb12
Sohail <sohail1613@gmail.com>
2022-04-18 16:42:03 +0530

    Build array

---
+++ src/com/dsa/questions/arrays/Main.java
@@ -0,0 +1,13 @@
+package com.dsa.questions.arrays;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+//        Questions que = new Questions();
+//
+//        int[] array = {0,2,1,5,3,4};
+//        int[] ans = que.buildArray(array);
+//        System.out.println(Arrays.toString(ans));
+    }
+}
---
+++ src/com/dsa/questions/arrays/Questions.java
@@ -0,0 +1,14 @@
+package com.dsa.questions.arrays;
+
+public class Questions {
+//    //Q1_Build Array from Permutation
+//    public int[] buildArray(int[] arr){
+//        int ans[] = new int[arr.length];
+//        for (int i = 0; i < arr.length ; i++) {
+//            ans[i] = arr[arr[i]];
+//        }
+//        return ans;
+//    }
+
+    
+}
