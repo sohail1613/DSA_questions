efb56f3dcb0a0eb2de99cb8711df61edc4bd42a1
Sohail <sohail1613@gmail.com>
2022-05-15 11:37:20 +0530

    Inheritance

--- src/com/oops/introduction/Inheritance/Animal.java
+++ src/com/oops/introduction/Inheritance/Animal.java
@@ -1,6 +1,5 @@
 package com.oops.introduction.Inheritance;
 //Single level inheritance
-
 class Animal {
     void eat(){
         System.out.println("Eating");
@@ -9,5 +8,19 @@ class Animal {
 class Dog extends Animal{
     void bark(){
         System.out.println("Barking");
+    }
+}
+
+//Multi-Level inheritance
+class BabyDog extends Dog{
+    void weep(){
+        System.out.println("Weeping");
+    }
+}
+
+//Hierarchical Inheritance (two or more classes inherits a single class)
+class Cat extends Animal{
+    void meow(){
+        System.out.println("meowing");
     }
 }
--- src/com/oops/introduction/Inheritance/Main.java
+++ src/com/oops/introduction/Inheritance/Main.java
@@ -1,9 +1,25 @@
 package com.oops.introduction.Inheritance;
 
 public class Main {
+    //single level inheritance
+//    public static void main(String[] args) {
+//        Dog d = new Dog();
+//        d.bark();
+//        d.eat();
+//    }
+
+    //multi-level inheritance
+//    public static void main(String[] args) {
+//        BabyDog b = new BabyDog();
+//        b.eat();
+//        b.bark();
+//        b.weep();
+//    }
+
+    //Hierarchical Inheritance
     public static void main(String[] args) {
-        Dog d = new Dog();
-        d.bark();
-        d.eat();
+        Cat c = new Cat();
+        c.eat();
+        c.meow();
     }
 }
