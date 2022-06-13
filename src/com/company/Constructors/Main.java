0159c4c0ff9af1cb16ab2f642bf4b64be305fb11
Sohail <sohail1613@gmail.com>
2022-06-13 13:27:19 +0530

    Invoking constructrs

---
+++ src/com/company/Constructors/Main.java
@@ -0,0 +1,7 @@
+package com.company.Constructors;
+
+public class Main {
+    public static void main(String[] args) {
+        InvokeConstructors invokeConstructors = new InvokeConstructors(5, 7);
+    }
+}
