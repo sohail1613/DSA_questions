664aff1c5bb6efdba4d1ef8589e59c40a1799ed2
Sohail <sohail1613@gmail.com>
2022-06-13 13:30:51 +0530

    Invoking Constructors

--- src/com/company/Constructors/InvokeConstructors.java
+++ src/com/company/Constructors/InvokeConstructors.java
@@ -1,15 +1,15 @@
 package com.company.Constructors;
 
 public class InvokeConstructors {
-     InvokeConstructors(int x, int y){ //these constructors will invoke from main method
-         this(5);//it will invoke one parameterized constructor
-         System.out.println(x+y);
-     }
-     InvokeConstructors(int x){
-         this();//it will invoke default constructor
-         System.out.println(x);
-     }
-     InvokeConstructors(){
-         System.out.println("Default constructor");
-     }
+    InvokeConstructors(int x, int y){
+        this(5);
+        System.out.println(x*y);
+    }
+    InvokeConstructors(int x){
+        this();
+        System.out.println(x);
+    }
+    InvokeConstructors(){
+        System.out.println("Default constructor");
+    }
 }
